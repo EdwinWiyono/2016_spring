d33fa48a51a6ad9d300918f45135c12efba8f0b8
Nguwi Yok Yen <yokyen@gmail.com>
2016-07-25 09:44:13 +0800

    added week2_demo1.py for if else condition

--- week2_demo1.py
+++ week2_demo1.py
@@ -1,5 +1,20 @@
-import math
+# import math
+#
+# radius = float(input("Enter a radius"))
+# area = 2 * math.pi * radius
+# print("The area is ", area)
 
-radius = float(input("Enter a radius"))
-area = 2 * math.pi * radius
-print("The area is ", area)
+#Revision on if-elif-else
+temperature = float(input("Enter the temperature today:"))
+
+if temperature > 35:
+    if temperature == 36:
+        print("It is very hot!")
+    else:
+        print("It is horrible!")
+    print("It is hot anyway!")
+elif temperature > 33:
+    print("Don't go outdoor")
+else:
+    print("It is not too bad")
+print("Always printed")
